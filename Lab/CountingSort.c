2270e101ebee34d8cd433be5ed1e8f4b84a49361
Rishaan <91053141+rishn@users.noreply.github.com>
2022-11-22 14:42:59 +0530

    Update CountingSort.c

--- Lab/CountingSort.c
+++ Lab/CountingSort.c
@@ -13,24 +13,16 @@ int main()
     }
     int *count = (int *)malloc(sizeof(int) * (max + 1)), *cum = (int *)malloc(sizeof(int) * (max + 1));
     for (i = 0; i <= max; i++)
+        count[i] = 0;
+    for (i = 0; i < n; i++)
+        count[arr[i]]++;
+    for (i = 1; i <= max; i++)
+        count[i] += count[i - 1];
+    for (i = n - 1; i >= 0; i--)
+        cum[--count[arr[i]]] = arr[i];
+    for (i = 0; i < n; i++)
     {
-        int c = 0;
-        cum[i] = t;
-        for (j = 0; j < n; j++)
-            if (i == arr[j])
-                c++;
-        count[i] = c;
-        t += c;
-    }
-    for (i = 0; i < max; i++)
-        for (j = cum[i]; j < cum[i + 1]; j++)
-        {
-            arr[j] = i;
-            printf("%d ", arr[j]);
-        }
-    for (i = cum[max]; i < n; i++)
-    {
-        arr[i] = max;
+        arr[i] = cum[i];
         printf("%d ", arr[i]);
     }
     return 0;
